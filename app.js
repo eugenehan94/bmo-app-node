99b8ed551f1197e8b81c8a984fb5e6ab39655642
eugenehan94 <eugene.han94@gmail.com>
2024-01-02 18:53:26 -0500

    added cors

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const personal = require("./routes/personal");
 const business = require("./routes/business");
 const port = process.env.PORT || 5000;
 
-app.use("/api/personal/home", personal);
-app.use("/api/business/home", business);
+// Enabling CORS for some specific origins only.
+let corsOptions = {
+  origin: "http://localhost:4200",
+};
+app.use(cors(corsOptions));
+// Routes
+app.use("/api/v1/personal/home", personal);
+app.use("/api/v1/business/home", business);
 
 app.listen(port, () => {
   console.log(`app is listening on port ${port}`);
