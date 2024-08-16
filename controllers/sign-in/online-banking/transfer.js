2160777dba4c30f6de5e7ff14348e368dad76c21
eugenehan94 <eugene.han94@gmail.com>
2024-08-16 19:55:53 -0400

    added queries to update the amounts of the two transfer fund accounts

--- controllers/sign-in/online-banking/transfer.js
+++ controllers/sign-in/online-banking/transfer.js
@@ -1,10 +1,22 @@
 const mySqlConnection = require("../../../libraries/mysql");
 const transferFunds = async (req, res, next) => {
   console.log("result: ", req.body);
-  const { customerId } = req.body;
+  const {
+    fromAccountAmount,
+    fromAccount,
+    toAccountAmount,
+    toAccount,
+    customerId,
+  } = req.body;
 
   let userAccounts;
 
+  mySqlConnection.query(
+    `UPDATE bmo_project.accounts SET Amount=${fromAccountAmount} WHERE AccountNumber=${fromAccount} AND CustomerID=${customerId}`
+  );
+  mySqlConnection.query(
+    `UPDATE bmo_project.accounts SET Amount=${toAccountAmount} WHERE AccountNumber=${toAccount} AND CustomerID=${customerId}`
+  );
   mySqlConnection.query(
     `SELECT * FROM bmo_project.accounts WHERE CustomerID = ${customerId}`,
     (error, result) => {
