e7c3e496253c21a04757188de9456f3748cda93a
eugenehan94 <eugene.han94@gmail.com>
2024-09-07 14:11:39 -0400

    added code to find customerID after user generated

--- controllers/open-account/home.js
+++ controllers/open-account/home.js
@@ -1,4 +1,5 @@
 const mySqlConnection = require("../../libraries/mysql");
+
 const generateRandomText = (length) => {
   const characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -8,45 +9,80 @@ const generateRandomText = (length) => {
   }
   return result;
 };
+
 const createAccount = async (req, res, next) => {
   try {
     const { firstName, lastName } = req.body;
     console.log("firstName: ", firstName, " lastName: ", lastName);
 
     let randomPassword = generateRandomText(4);
-    console.log("random password: ", randomPassword);
-    uniqueCardNumber();
-    res.status(200).json({ msg: "User created" });
-    // Check if randomCardNumber is unique in database
+    let randomCardNumber = await uniqueCardNumber();
 
-    // const createUser = await mySqlConnection.execute(
-    //   `INSERT INTO bmo_project.customer (LastName, FirstName, CardNumber, Password) VALUES (${lastName}, ${firstName}, ${randomCardNumber}, ${randomPassword})`
-    // );
+    mySqlConnection.query(
+      `INSERT INTO bmo_project.customer (LastName, FirstName, CardNumber, Password) VALUES ("${lastName}", "${firstName}", ${randomCardNumber}, "${randomPassword}")`,
+      (error, result) => {
+        if (error) {
+          console.log("error: ", error);
+        }
+      }
+    );
+
+    // Get Id from database and create two random accounts with random amounts
+    mySqlConnection.query(
+      `SELECT CustomerID FROM bmo_project.customer WHERE CardNumber =${randomCardNumber} `,
+      (error1, results1) => {
+        if (error1) {
+          throw error1;
+        }
+        let randomAmountOne = Math.floor(Math.random() * 1000) + 1;
+        let randomAmountTwo = Math.floor(Math.random() * 100) + 1;
+        //
+        console.log("result1: ", results1);
+        console.log("result1: ", results1[0].CustomerID);
+        // mySqlConnection.query(
+        //   `INSERT INTO bmo_project.accounts (CustomerID, Amount, AccountType, AccountNumber) VALUES (${results1}, ${randomAmountOne}, 'Chequing', 1234 )`
+        // );
+
+        // mySqlConnection.query(
+        //   `INSERT INTO bmo_project.accounts (CustomerID, Amount, AccountType, AccountNumber) VALUES (${results1}, ${randomAmountTwo}, 'Saving', 1234 )`
+        // );
+      }
+    );
+    res.status(200).json({ msg: "User created" });
   } catch (error) {
     res.status(500).json({ msg: "error" });
   }
 };
 
 const uniqueCardNumber = async () => {
-  mySqlConnection.query(
-    `SELECT CardNumber FROM bmo_project.customer`,
-    (error, result) => {
-      if (error) {
-        console.log("Error");
-        throw error;
+  let randomCardNumber;
+
+  return new Promise((resolve, reject) => {
+    mySqlConnection.query(
+      `SELECT CardNumber FROM bmo_project.customer`,
+      (error, result) => {
+        if (error) {
+          console.log("Error");
+          reject(error);
+        } else {
+          let isUnique = false;
+
+          while (!isUnique) {
+            randomCardNumber = Math.floor(Math.random() * 10000) + 1;
+            for (let i = 0; i < result.length; i++) {
+              if (result[i].CardNumber === randomCardNumber) {
+                isUnique = false;
+                break;
+              } else {
+                isUnique = true;
+              }
+            }
+          }
+          resolve(randomCardNumber);
+        }
       }
-      let isUnique = false;
-      let randomCardNumber;
-      // while (!isUnique) {
-      //   randomCardNumber = Math.floor(Math.random() * 10000) + 1;
-      // }
-      for (let i = 0; i < result.length; i++) {
-        console.log("for loop results: ", result[i].CardNumber);
-        console.log("logic test: ", result[i].CardNumber === 123);
-      }
-      console.log("table results: ", result);
-    }
-  );
+    );
+  });
 };
 module.exports = {
   createAccount,
